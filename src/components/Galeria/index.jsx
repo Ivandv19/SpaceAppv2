bbbdf63b5134c005d7fb6bc55238da63f955e7fc
ivandev <ivangtx19@gmail.com>
2024-09-04 11:02:47 -0600

    fix: Intenta arreglar error al desplegar en vercel

--- src/components/Galeria/index.jsx
+++ src/components/Galeria/index.jsx
@@ -5,7 +5,7 @@ import Tag from "./Tags";
 import { useContext } from "react";
 import { GlobalContext } from "../../context/GlobalContext";
 import Cargando from "../Cargando";
-import MostrarFotos from './MostrarFotos'
+import MostrarFotos from './Mostrarfotos'
 
 
 // Contenedor principal de la galería con estilos
